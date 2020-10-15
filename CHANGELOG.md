14e9bfd8630cd8260a8e99253cebdc3fa9c5a407
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-10-15 12:41:10 +0200

    Updated example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.0.1+1
+## 0.0.1+2
 
-* Updated Android implementation
 * Updated Readme
+* Updated example
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -58,7 +58,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('MPush Example'),
         ),
         body: Container(),
       ),
